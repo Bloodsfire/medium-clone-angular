e520436328baf2c4a8bb32e1fe4999d2b4eca6db
Bloodsfire <voobsheeee@mail.ru>
2020-12-28 16:18:47 +0600

    Create auth service

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -4,6 +4,8 @@ import { select, Store } from '@ngrx/store'
 import { registerAction } from '../../store/actions/register.action'
 import { Observable } from 'rxjs'
 import { isSubmittingSelector } from '../../store/selectors'
+import { AuthService } from '../../services/auth.service'
+import { CurrentUserInterface } from '../../../shared/types/currentUser.interface'
 
 @Component({
   selector: 'mc-register',
@@ -14,7 +16,11 @@ export class RegisterComponent implements OnInit {
   form: FormGroup
   isSubmitting$: Observable<boolean>
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm()
@@ -36,5 +42,10 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     this.store.dispatch(registerAction(this.form.value))
+    this.authService
+      .register(this.form.value)
+      .subscribe((currentUser: CurrentUserInterface) => {
+        console.log('currentUser: ', currentUser)
+      })
   }
 }
