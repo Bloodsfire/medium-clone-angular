2593450b03e4c4320a43ba477f4b9b955e09d621
Bloodsfire <voobsheeee@mail.ru>
2020-12-30 15:32:31 +0600

    Create feed module

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { GlobalFeedComponent } from './components/globalFeed/globalFeed.component'
 import { RouterModule } from '@angular/router'
+import { FeedModule } from '../shared/modules/feed/feed.module'
 
 const routes = [
   {
@@ -12,7 +13,7 @@ const routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), FeedModule],
   declarations: [GlobalFeedComponent],
 })
 export class GlobalFeedModule {}
