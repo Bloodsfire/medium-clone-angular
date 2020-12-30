e2aa7a709e997d92cce0994c314dbf8c4ef43126
Bloodsfire <voobsheeee@mail.ru>
2020-12-30 14:03:38 +0600

    Create getCurrentUser effect

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -13,6 +13,7 @@ import { RegisterEffect } from './store/effects/register.effect'
 import { LoginEffect } from './store/effects/login.effect'
 import { BackendErrorMessagesModule } from '../shared/modules/backendErrorMessages/backendErrorMessages.module'
 import { LoginComponent } from './components/login/login.component'
+import { getCurrentUserEffect } from './store/effects/getCurrentUser.effect'
 
 const routes: Routes = [
   {
@@ -31,7 +32,11 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ReactiveFormsModule,
     StoreModule.forFeature('auth', reducers),
-    EffectsModule.forFeature([RegisterEffect, LoginEffect]),
+    EffectsModule.forFeature([
+      RegisterEffect,
+      LoginEffect,
+      getCurrentUserEffect,
+    ]),
     BackendErrorMessagesModule,
   ],
   declarations: [RegisterComponent, LoginComponent],
