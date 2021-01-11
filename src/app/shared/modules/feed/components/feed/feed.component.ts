33ae168c2e7226ec64ae267a8e5a1e506d057110
Bloodsfire <voobsheeee@mail.ru>
2021-01-11 14:48:27 +0600

    Create feed pagination variables

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute, Params, Router } from '@angular/router'
 import { select, Store } from '@ngrx/store'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 
+import { environment } from '../../../../../../environments/environment'
 import { getFeedAction } from '../../store/actions/getFeed.action'
 import { GetFeedResponseInterface } from '../../types/getFeedResponse.interface'
 import {
@@ -15,24 +17,48 @@ import {
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps: string
 
   isLoading$: Observable<boolean>
   error$: Observable<string | null>
   feed$: Observable<GetFeedResponseInterface | null>
+  queryParamsSubscription: Subscription
+  limit = environment.limit
+  baseUrl: string
+  currentPage: number
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues()
+    this.initializeListeners()
     this.fetchData()
+    console.log('baseUrl', this.router.url)
+  }
+
+  ngOnDestroy() {
+    this.queryParamsSubscription.unsubscribe()
   }
 
   initializeValues(): void {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
     this.error$ = this.store.pipe(select(errorSelector))
     this.feed$ = this.store.pipe(select(feedSelector))
+    this.baseUrl = this.router.url.split('?')[0]
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params.page || '1')
+        console.log('currentPage: ', this.currentPage)
+      }
+    )
   }
 
   fetchData(): void {
