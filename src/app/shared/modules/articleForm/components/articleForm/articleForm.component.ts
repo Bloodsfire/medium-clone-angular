82870fc5cbc1052cc21d8b48187c0a5eb397ece1
Bloodsfire <voobsheeee@mail.ru>
2021-01-12 14:54:26 +0600

    Create articleForm module

---
+++ src/app/shared/modules/articleForm/components/articleForm/articleForm.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core'
+
+import { ArticleInputInterface } from '../../../../types/articleInput.interface'
+import { BackendErrorsInterface } from '../../../../types/backendErrors.interface'
+
+@Component({
+  selector: 'mc-article-form',
+  templateUrl: 'articleForm.component.html',
+})
+export class ArticleFormComponent {
+  @Input('initialValues') initialValuesProps: ArticleInputInterface
+  @Input('isSubmitting') isSubmittingProps: boolean
+  @Input('errors') errorsProps: BackendErrorsInterface | null
+}
