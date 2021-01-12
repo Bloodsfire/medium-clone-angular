77fefdc66f044bc64bf029acadf00fe6e0a9a10a
Bloodsfire <voobsheeee@mail.ru>
2021-01-12 16:13:53 +0600

    Create createArticle service

--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -2,10 +2,12 @@ import { AuthStateInterface } from '../../auth/types/authState.interface'
 import { FeedStateInterface } from '../modules/feed/types/feedState.interface'
 import { PopularTagsStateInterface } from '../modules/popularTags/types/popularTagsState.interface'
 import { ArticleInterface } from './article.interface'
+import { CreateArticleStateInterface } from '../../createArticle/types/createArticleState.interface'
 
 export interface AppStateInterface {
   auth: AuthStateInterface
   feed: FeedStateInterface
   popularTags: PopularTagsStateInterface
   article: ArticleInterface
+  createArticle: CreateArticleStateInterface
 }
